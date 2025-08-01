024d23d5a043a4ebc894453bc1a395c7ed73941c
bradudenis95 <bradudenis95@gmail.com>
2025-08-01 11:28:52 -0700

    Refactor database connection configuration in db.ts

--- server/app/db.ts
+++ server/app/db.ts
@@ -3,10 +3,7 @@ import { Pool } from 'pg';
 import * as schema from '../db/schema';
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL
 });
 
 export const db = drizzle(pool, { schema });
